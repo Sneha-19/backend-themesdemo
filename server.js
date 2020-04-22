1183507fe05d8f2d1195d1d54a507b010763d974
Sneha Singh <sneha19.osl@gmail.com>
2020-04-22 11:57:54 +0530

    make it better

--- server.js
+++ server.js
@@ -11,13 +11,19 @@ var md5 = require("md5")
 var bodyParser = require('body-parser');
 
 //mounts BodyParser as middleware - every request passes through it
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Start server
-app.listen(port, function(){
-    console.log('App started on port '+port);
+app.listen(port, function () {
+    console.log('App started on port ' + port);
 });
+
+
+app.get('/', function (request, response) {
+    response.send('Hello, World!');
+})
+
 
 //get a list of users
 app.get("/users", (req, res, next) => {
@@ -25,14 +31,14 @@ app.get("/users", (req, res, next) => {
     var params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
             // "message":"success",
-            "data":rows
+            "data": rows
         })
-      });
+    });
 });
 
 //api to check revenue data
@@ -41,14 +47,14 @@ app.get("/revenues", (req, res, next) => {
     var params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
             // "message":"success",
-            "data":rows
+            "data": rows
         })
-      });
+    });
 });
 
 //API to check earnings table data
@@ -57,14 +63,14 @@ app.get("/earnings", (req, res, next) => {
     var params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
             // "message":"success",
-            "data":rows
+            "data": rows
         })
-      });
+    });
 });
 
 
@@ -74,45 +80,45 @@ app.get("/users/:userId", (req, res, next) => {
     var params = [req.params.userId]
     db.get(sql, params, (err, row) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
             // "message":"success",
-            "data":row
+            "data": row
         })
-      });
+    });
 });
 
 //Creating a new user
 app.post("/postFormData/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
+    var errors = []
+    if (!req.body.password) {
         errors.push("No password specified");
     }
-    if (!req.body.email){
+    if (!req.body.email) {
         errors.push("No email specified");
     }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
         return;
     }
     var data = {
         userName: req.body.userName,
         email: req.body.email,
-        password : md5(req.body.password)
+        password: md5(req.body.password)
     }
-    var sql ='INSERT INTO userData (userName, email, password) VALUES (?,?,?)'
-    var params =[data.userName, data.email, data.password]
+    var sql = 'INSERT INTO userData (userName, email, password) VALUES (?,?,?)'
+    var params = [data.userName, data.email, data.password]
     db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
+        if (err) {
+            res.status(400).json({ "error": err.message })
             return;
         }
         res.json({
             // "message": "success",
             "data": data,
-            "userId" : this.lastID
+            "userId": this.lastID
         })
     });
 })
@@ -120,36 +126,36 @@ app.post("/postFormData/", (req, res, next) => {
 //POST API for add income form
 app.post('/addIncomeForm/', (req, res, next) => {
     var errors = []
-    if(!req.body.timestamp){
+    if (!req.body.timestamp) {
         errors.push("No Date specified");
     }
-    if(!req.body.userId){
+    if (!req.body.userId) {
         errors.push("No User Id specified");
     }
-    if(!req.body.revenueId){
+    if (!req.body.revenueId) {
         errors.push("Revenue Id not specified");
     }
-    if(!req.body.earningAmount){
+    if (!req.body.earningAmount) {
         errors.push("Earning Amount not specified");
     }
-    if(errors.length){
-        res.status(400).json({"errors":error.join(",")});
+    if (errors.length) {
+        res.status(400).json({ "errors": error.join(",") });
     }
     var data = {
         timestamp: req.body.timestamp,
-        userId : req.body.userId,
-        revenueId : req.body.revenueId,
-        earningAmount : req.body.earningAmount
+        userId: req.body.userId,
+        revenueId: req.body.revenueId,
+        earningAmount: req.body.earningAmount
     }
     var sql = 'INSERT INTO earnings (timestamp, userId, revenueId, earningAmount) VALUES (?,?,?,?)'
     var params = [data.timestamp, data.userId, data.revenueId, data.earningAmount]
-    db.run(sql, params, function(err, result) {
-        if(err) {
-            res.status(400).json({"error":err.message})
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
             return;
         }
         res.json({
-            "data":data
+            "data": data
         })
     });
 })
@@ -160,14 +166,14 @@ app.get("/getMonthwiseEarnings/:userId", (req, res, next) => {
     var params = [req.params.userId]
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
             // "message":"success",
-            "data":rows
+            "data": rows
         })
-      });
+    });
 })
 
 //GET API to get revenue type
@@ -175,12 +181,12 @@ app.get('/getRevenueData/:revenueId', (req, res, next) => {
     var sql = "SELECT revenueType FROM revenueData CROSS JOIN earnings ON earnings.revenueId = revenueData.revenueId WHERE earnings.revenueId = ?"
     var params = [req.params.revenueId]
     db.get(sql, params, (err, row) => {
-        if(err) {
-            res.status(400).json({"error":err.message});
+        if (err) {
+            res.status(400).json({ "error": err.message });
             return;
         }
         res.json({
-            "data":row
+            "data": row
         })
     });
 })
